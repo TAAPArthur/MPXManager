6f58acb9f79964aef8a065723ad5cee7fe78630f
TAAPArthur <taaparthur@gmail.com>
2019-06-30 15:23:37 -0500

    Improved autocomplete

--- src/mpxmanager-autocomplete.sh
+++ src/mpxmanager-autocomplete.sh
@@ -7,10 +7,11 @@ _mpxmanagerAutocomplete ()   #  By convention, the function name
 
     COMPREPLY=()   # Array variable storing the possible completions.
     cur=${COMP_WORDS[COMP_CWORD]}
+    last=${COMP_WORDS[COMP_CWORD-1]}
     firstArg=${COMP_WORDS[1]}
 
-    sendOptions="--dump --dump-all --dump-filter --dump-win --list-options --log-level --quit --restart --sum --auto_focus_new_window_timeout --clone_refresh_rate --crash_on_errors --default_border_color --default_border_width --default_focus_border_color --default_mode --default_mod_mask --default_number_of_hidden_workspaces --default_number_of_workspaces --default_unfocus_border_color --default_workspace_index --event_period --ignore_key_repeat --ignore_mask --ignore_send_event --ignore_subwindows --kill_timeout --ld_preload_injection --load_saved_state --master_info_path --poll_count --poll_interval --run_as_wm --run_as_wm --shell --steal_wm_selection --sync_focus --sync_focus --sync_window_masks"
-    if [[ "$firstArg" == "--set" ||"$firstArg" == "--send" || "$firstArg" == "-s" ]]; then
+    sendOptions="dump dump-all dump-filter dump-win list-options log-level quit restart sum auto_focus_new_window_timeout clone_refresh_rate crash_on_errors default_border_color default_border_width default_focus_border_color default_mode default_mod_mask default_number_of_hidden_workspaces default_number_of_workspaces default_unfocus_border_color default_workspace_index event_period ignore_key_repeat ignore_mask ignore_send_event ignore_subwindows kill_timeout ld_preload_injection load_saved_state master_info_path poll_count poll_interval run_as_wm shell spawn_env steal_wm_selection sync_focus sync_window_masks"
+    if [[ "$last" == "--set" || "$last" == "--send" || "$last" == "-s" ]]; then
         COMPREPLY=( $( compgen -W "$sendOptions --" -- $cur ) )
         return 0;
     else
